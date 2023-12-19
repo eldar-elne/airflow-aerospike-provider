177eda89992be540366cab078b900dfef4848820
Eldar Elnekave <eldar.elnekave@bigabid.com>
2023-12-19 20:53:48 +0200

    Return type on `AerospikeHook` Updated doc string

--- aerospike_provider/operators/aerospike.py
+++ aerospike_provider/operators/aerospike.py
@@ -12,13 +12,14 @@ from airflow.models.baseoperator import BaseOperator
 
 class AerospikePutKey(BaseOperator):
     """
-    Create a new record, add or remove bins or remove a record using `<bin_name>.isnull()`.
+    Create a new record, add or remove bins.
+    This can also remove a record (if exists) using `<bin_name>.isnull()` if it's the last bin.
 
     :param aerospike_conn_id: aerospike connection to use, defaults to 'aerospike_default'
     :param key: key to save in the db.
     :param namespace: namespace to use in aerospike db
     :param set: set name in the namespace
-    :param bins: bin names with data saved along a key. For example: `{"bin": value}`
+    :param bins: bin name(s) with data saved along with a key. For example: `{"bin": value}`
     :param metadata: metadata about the key eg. ttl. For example: `{"ttl": 0}`
     :param policy: which policy the key should be saved with. default `POLICY_KEY_SEND`
     """
@@ -32,7 +33,7 @@ class AerospikePutKey(BaseOperator):
         namespace: str,
         set: str,
         key: str,
-        bins: dict, 
+        bins: dict,
         metadata: dict = None,
         policy: dict = {'key': aerospike.POLICY_KEY_SEND},
         aerospike_conn_id: str = "aerospike_default",
@@ -48,7 +49,7 @@ class AerospikePutKey(BaseOperator):
         self.policy = policy
         self.aerospike_conn_id = aerospike_conn_id
     
-    def execute(self, context: Context) -> bool:
+    def execute(self, context: Context) -> None:
         
         hook = AerospikeHook(self.aerospike_conn_id)
         self.log.info('Storing %s as key', self.key)
